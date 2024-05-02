68b8f212cfaeaa7731840d4fb51738eb0b641c12
naufaladysaputro <naufaladysaputro@gmail.com>
2024-05-02 23:32:23 +0700

    first commit

---
+++ app/meals/error.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An error occured</h1>
+            <p>failed to fetch meals data. Please reload ao try agai later.</p>
+        </main>
+    )
+}
